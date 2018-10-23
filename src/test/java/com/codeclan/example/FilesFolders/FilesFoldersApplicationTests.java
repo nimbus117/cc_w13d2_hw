dcf3dc23930e7ade9e274b9a461fc6c4183a6d47
nimbus117 <mail@nimbus117.co.uk>
2018-10-23 15:40:15 +0100

    added quick test of classes

--- src/test/java/com/codeclan/example/FilesFolders/FilesFoldersApplicationTests.java
+++ src/test/java/com/codeclan/example/FilesFolders/FilesFoldersApplicationTests.java
@@ -1,9 +1,14 @@
 package com.codeclan.example.FilesFolders;
 
+import com.codeclan.example.FilesFolders.models.File;
+import com.codeclan.example.FilesFolders.models.Folder;
+import com.codeclan.example.FilesFolders.models.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +16,14 @@ public class FilesFoldersApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testClasses() {
+		User user = new User("Bob");
+		Folder folder = new Folder("MyDocs", user);
+		File file = new File("cv", "txt", 10, folder);
+		assertEquals(10, file.getSize());
 	}
 
 }
