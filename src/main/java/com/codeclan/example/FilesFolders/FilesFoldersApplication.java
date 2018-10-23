e04221ab8ee45f0b75f62c44474b84ff624001f8
nimbus117 <mail@nimbus117.co.uk>
2018-10-23 15:07:33 +0100

    Initial commit

---
+++ src/main/java/com/codeclan/example/FilesFolders/FilesFoldersApplication.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.FilesFolders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesFoldersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilesFoldersApplication.class, args);
+	}
+}
