c5a6ef8f4a5a53b6d1afda4aea3b90f98892c42e
nimbus117 <mail@nimbus117.co.uk>
2018-10-23 15:32:04 +0100

    added folder class

--- src/main/java/com/codeclan/example/FilesFolders/models/Folder.java
+++ src/main/java/com/codeclan/example/FilesFolders/models/Folder.java
@@ -1,4 +1,52 @@
 package com.codeclan.example.FilesFolders.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Folder {
+    private Long id;
+    private String title;
+    private User user;
+    private List<File> files;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.user = user;
+        this.files = new ArrayList<>();
+    }
+
+    public Folder() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
 }
